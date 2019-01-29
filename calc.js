fda90e695270ee82c413be6084c7bf22bd77ee7d
cptchuckles <image1996@gmail.com>
2019-01-28 21:50:45 -0600

    Add working backspace key

--- calc.js
+++ calc.js
@@ -1,10 +1,33 @@
 let output = document.querySelector("#output");
+let op = document.querySelector("#op");
 
-document.querySelectorAll(".numpad button, .oper").forEach(b => {
+document.querySelectorAll(".num").forEach(b => {
     b.addEventListener("click", () => {
         output.textContent += b.textContent;
     });
 });
 
-document.querySelector("#cls").addEventListener("click", 
-                                () => output.textContent = "");
+document.querySelector("#bs").addEventListener("click", () => {
+    let n = output.textContent;
+    output.textContent = n.slice(0, n.length-1);
+});
+
+document.querySelectorAll(".oper").forEach(oper => {
+    oper.addEventListener("click", () => {
+        op.textContent = oper.textContent;
+    });
+});
+
+document.querySelector("#cls")
+        .addEventListener("click", () => {
+            output.textContent = "";
+            op.textContent = "";
+        });
+
+document.querySelector("#eval").addEventListener("click",
+                                () => calc(output.textContent));
+
+
+function calc(input) {
+    //TODO: implement this
+}
