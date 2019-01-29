d29438aebf553d8f80463e16fa605c34ffbd42de
cptchuckles <image1996@gmail.com>
2019-01-29 15:14:45 -0600

    Finish input functionality
    Went back to string-equation format. Going to have to
    git gud and learn how to tokenize a string for operators and
    treat them with priority and shit.

--- calc.js
+++ calc.js
@@ -1,27 +1,21 @@
-let output = document.querySelector("#output");
-let op = document.querySelector("#op");
+const regOperAll = /[\+\-\*\/]/g;
+const operators = "/+-*";
 
-document.querySelectorAll(".num").forEach(b => {
+let display = document.querySelector("#display");
+let eq = '';
+
+document.querySelectorAll(".num, .oper").forEach(b => {
     b.addEventListener("click", () => {
-        output.textContent += b.textContent;
+        appendChar(b.textContent);
     });
 });
 
-document.querySelector("#bs").addEventListener("click", () => {
-    let n = output.textContent;
-    output.textContent = n.slice(0, n.length-1);
-});
-
-document.querySelectorAll(".oper").forEach(oper => {
-    oper.addEventListener("click", () => {
-        op.textContent = oper.textContent;
-    });
-});
+document.querySelector("#bs").addEventListener("click", delChar);
 
 document.querySelector("#cls")
         .addEventListener("click", () => {
-            output.textContent = "";
-            op.textContent = "";
+            eq = "";
+            updateDisplay(eq);
         });
 
 document.querySelector("#eval").addEventListener("click",
@@ -35,16 +29,64 @@ function calc(input) {
 }
 
 
-function punctualize(string) {
-    if( isNaN(string) ) return "ERROR";
-    let sides = string.split('.');
-    let left = sides[0];
-    let right = '';
-    if (sides.length === 2) right = sides[1];
+function punctualize(equation) {
+    let nums = equation.split(regOperAll);
+    let ops = equation.match(regOperAll);
 
-    for(i = left.length-3; i > 0; i -= 3) {
-        left = left.slice(0, i) + ',' + left.slice(i);
+    nums = nums.map(string => {
+        if(string.length === 0) return string;
+        if( isNaN(string) ) {
+            if(string==='.') return '.';
+            else return "ERROR";
+        }
+
+        let sides = string.split('.');
+        let left = sides[0];
+        let right = '';
+        if (sides.length === 2) right = '.' + sides[1];
+
+        for(i = left.length-3; i > 0; i -= 3) {
+            left = left.slice(0, i) + ',' + left.slice(i);
+        }
+
+        return right.length === 0 ? left : left + right;
+    });
+
+    let j=0;
+    return nums.reduce((a, s) => a + ops[j++] + s);
+}
+
+function addDot(string) {
+    let i = string.length;
+    do {
+        i--;
+    } while( ! operators.includes(string[i]) && i > 0 );
+
+    if( string.slice(i).includes('.') ) return false;
+
+    return true;
+}
+
+function updateDisplay(string) {
+    display.textContent = punctualize(string);
+}
+
+function appendChar(c) {
+    if(operators.includes(c)) {
+        let last = eq.slice(-1);
+        if(operators.includes(last)) {
+            if(c==='-' && last==='-' || c!=='-') return;
+        }
+    }
+    if(c === '.') {
+        if( ! addDot(eq) ) return;
     }
 
-    return right.length === 0 ? left : left + '.' + right;
+    eq += c;
+    updateDisplay(eq);
+}
+
+function delChar() {
+    eq = eq.slice(0, eq.length - 1);
+    updateDisplay(eq);
 }
